9d731325b32243a5f70bc20a95921902f97bd0ba
krueger <catalinpecingine@gmail.com>
2024-02-05 12:45:49 +0200

    d

--- src/test/resources/installChrome.sh
+++ src/test/resources/installChrome.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chromedriver-linux64.zip
-sudo apt install unzip && unzip ./chromedriver-linux64.zip && sudo apt install ./chromedriver-linux64/chromedriver
+sudo apt install unzip && unzip ./chromedriver-linux64.zip && sudo apt install ./chromedriver-linux64/
