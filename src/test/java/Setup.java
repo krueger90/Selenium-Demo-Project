bdd2d977dacb6c5ea3db897ccbcf6ed90679ac02
krueger <catalinpecingine@gmail.com>
2024-02-06 10:47:39 +0200

    test

--- src/test/java/Setup.java
+++ src/test/java/Setup.java
@@ -2,21 +2,15 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Setup {
 
-    protected WebDriver driver = new FirefoxDriver();
-    FirefoxOptions options = new FirefoxOptions();
+    protected WebDriver driver = new ChromeDriver();
 
     Wait<WebDriver> wait = new FluentWait<>(driver)
             .withTimeout(Duration.ofSeconds(5))
@@ -24,24 +18,18 @@ public class Setup {
 
     @BeforeMethod
     public void setup() {
-          	 WebDriverManager.firefoxdriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+            options.setBinary("/usr/bin/google-chrome");
             options.addArguments("--no-sandbox");
             options.addArguments("--headless=new");
-            options.addArguments("--start-maximized");
-            options.addArguments("--ignore-certificate-errors");
             options.addArguments("--disable-dev-shm-usage");
-            driver = new FirefoxDriver(options);
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
-
             driver.get("https://ecommerce-playground.lambdatest.io");
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         
     }
-
-    // @BeforeMethod
-    // public void openBrowser() {
-    // driver.get("https://ecommerce-playground.lambdatest.io");
-    // }
 
     @AfterMethod
     public void clearCookies() {
--- src/test/resources/installChrome.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/123.0.6272.2/linux64/chromedriver-linux64.zip
-sudo apt install unzip && unzip ./chromedriver-linux64.zip
